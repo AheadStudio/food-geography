b88c8747279a2a163945ae83365a782d400fe5a4
VictorPullzz <GORPONFFFFFF@yandex.ru>
2018-05-27 21:45:42 +0400

    First commit

---
+++ dev/js/foodgeography.js
@@ -0,0 +1,14 @@
+(function($) {
+	var FOODGEOGRAPHY = (function() {
+
+		var $sel = {};
+		$sel.window = $(window);
+		$sel.html = $("html");
+		$sel.body = $("body", $sel.html);
+
+		return {
+
+		};
+
+	})();
+})(jQuery);
